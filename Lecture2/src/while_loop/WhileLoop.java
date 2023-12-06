e01ae457d9ed5407aa17d91fa801152314bea25a
Ayushh005 <147608092+Ayushh005@users.noreply.github.com>
2023-12-06 23:59:09 +0530

    Initial commit

--- Lecture2/src/while_loop/WhileLoop.java
+++ Lecture2/src/while_loop/WhileLoop.java
@@ -1,4 +1,14 @@
 package while_loop;
+import java.util.Scanner;
 
 public class WhileLoop {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n  = s.nextInt();
+        int i = 1;
+        while (i<=n) {
+            System.out.println(i);
+              i= i+3;
+        }
+    }
 }
