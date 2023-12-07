35a51aa26db42c444ec5a9a86662c5d3b0c724fe
Ayushh005 <147608092+Ayushh005@users.noreply.github.com>
2023-12-07 22:42:17 +0530

    initial commit

--- Lecture3/src/while_loop/WhileLoop.java
+++ Lecture3/src/while_loop/WhileLoop.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class WhileLoop {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter an Integer =");
         int n = s.nextInt();
         int i = 1;
         while(i<=n) {
             System.out.println(i);
             i = i + 1;
         }
+        System.out.println("Here's the answer...");
     }
 }
