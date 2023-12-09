1e5620a2a444d52f703ff454c53118daee907ce0
Ayush <ayushofficial2323@gmail.com>
2023-12-09 14:57:23 +0530

    commmit

---
+++ Lecture2/src/while_loop/TryTest.java
@@ -0,0 +1,10 @@
+package while_loop;
+
+public class TryTest {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 14;
+        int c = a+b;
+        System.out.println(a+b);
+    }
+}
