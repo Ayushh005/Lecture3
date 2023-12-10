88f114ff2ae31dcfdaf7cad80e2614f7d4398226
Ayush <ayushofficial2323@gmail.com>
2023-12-10 22:59:38 +0530

    commmit

--- Lecture2/src/while_loop/TryTest.java
+++ Lecture2/src/while_loop/TryTest.java
@@ -1,10 +1,14 @@
 package while_loop;
-
+import java.util.Scanner;
 public class TryTest {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 14;
+        Scanner s = new Scanner(System.in);
+        System.out.print("a=");
+        int a = s.nextInt();
+        System.out.print("b=");
+        int b = s.nextInt();
+
         int c = a+b;
-        System.out.println(a+b);
+        System.out.println("a+b="+c);
     }
 }
