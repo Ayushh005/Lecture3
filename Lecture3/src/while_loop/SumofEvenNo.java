b8304a0c269c71cd46d899e69731d89205f73d10
Ayush <ayushofficial2323@gmail.com>
2023-12-11 22:15:17 +0530

    initial commit

---
+++ Lecture3/src/while_loop/SumofEvenNo.java
@@ -0,0 +1,17 @@
+package while_loop;
+import java.util.Scanner;
+public class SumofEvenNo {
+    public static void main(String[] args) {
+        System.out.print("Enter an Integer = ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int sum = 0;
+        int i = 0;
+        while (i<=n) {
+            sum = sum + 1 ;
+            i = i + 2 ;
+        }
+        System.out.println(sum);
+    }
+}
