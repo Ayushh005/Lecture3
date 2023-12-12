cbd094c1b45634b785ac61e76ab9bdffc36e4aa4
Ayush <ayushofficial2323@gmail.com>
2023-12-12 21:00:52 +0530

    initial commit

---
+++ Lecture3/src/while_loop/FTCTable.java
@@ -0,0 +1,23 @@
+package while_loop;
+import java.util.Scanner;
+public class FTCTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Starting Fahrenheit Value = ");
+        int Start = sc.nextInt();
+        System.out.print("Enter End Fahrenheit Value = ");
+        int End = sc.nextInt();
+        System.out.print("Enter Step Size between them = ");
+        int Step = sc.nextInt();
+
+
+        int FTC = Start;
+        while (FTC <= End) {
+            int cel = (int)((5.0/9) * (FTC - 32));
+            System.out.println(FTC + "\t" + cel);
+            FTC += Step;
+        }
+
+
+    }
+}
