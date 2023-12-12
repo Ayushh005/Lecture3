5627bf1deca0ad2a1e096bdbf89d1002f5e6a4f3
Ayush <ayushofficial2323@gmail.com>
2023-12-12 22:55:41 +0530

    initial commit

---
+++ Lecture3/src/Patterns/Pattern2.java
@@ -0,0 +1,22 @@
+package Patterns;
+import java.util.Scanner;
+public class Pattern2 {
+    public static void main(String[] args) {
+        System.out.print("Enter size of Pattern = ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int i = 1;
+        int val = 1;
+        while (i<=n) {
+            int j = 1;
+            while (j<=i) {
+                System.out.print(val);
+                val = val + 1;
+                j = j + 1;
+            }
+            System.out.println();
+            i = i + 1;
+        }
+    }
+}
