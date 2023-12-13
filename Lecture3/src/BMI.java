b8e72e31ed6a8fae59cd0fafffa42738421cfd6f
Ayush <ayushofficial2323@gmail.com>
2023-12-13 10:30:10 +0530

    initial commit

--- Lecture3/src/BMI.java
+++ Lecture3/src/BMI.java
@@ -3,7 +3,7 @@ public class BMI {
     public static void main(String[] args) {
         Scanner s = new Scanner (System.in);
         System.out.print("Name=");
-        char name = s.next().charAt(0);
+        String name = s.next();
         System.out.print("Height=");
         double h = s.nextDouble();
         double ht = h/100;
@@ -11,7 +11,7 @@ public class BMI {
         double w = s.nextDouble();
 
         double BMI = w/(ht*ht);
-        System.out.println("Hello Sir...");
+        System.out.println("Hello " + name);
         System.out.println(" your BMI is "+BMI);
 
 
