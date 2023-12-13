6f0265b9c25dc17bc68a18fb8c930cd4ba00fbbb
Ayush <ayushofficial2323@gmail.com>
2023-12-13 22:11:11 +0530

    initial commit

---
+++ Lecture3/src/Patterns/RightAlignSum.java
@@ -0,0 +1,29 @@
+package Patterns;
+import java.util.Scanner;
+public class RightAlignSum {
+    public static void main(String[] args) {
+        System.out.print("Enter size of Pattern = ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int i = 1;
+        int val = 1;
+        while (i<=n)
+        {
+            int space = 1;
+            while (space<=n-i) {
+                System.out.print(" ");
+                space = space+1;
+            }
+            int j =1;
+            while (j<=i)
+            {
+                System.out.print(val);
+                val = val + 1;
+                j = j + 1;
+            }
+            i = i + 1;
+            System.out.println();
+        }
+    }
+}
