35e165e50cd414a106341e17521336b3d2f4e2cf
Ayush <ayushofficial2323@gmail.com>
2023-12-15 22:14:46 +0530

    initial commit

---
+++ Lecture3/src/Patterns/SquarePattern.java
@@ -0,0 +1,19 @@
+package Patterns;
+import java.util.Scanner;
+public class SquarePattern {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int i = 1;
+        while (i<=n) {
+            int j = 1;
+            while (j<=n) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
