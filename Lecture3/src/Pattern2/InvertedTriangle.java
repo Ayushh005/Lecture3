957e977fb8d6c4ea844e82e2de3677c5a7f8dc2e
Ayush <ayushofficial2323@gmail.com>
2023-12-17 17:07:01 +0530

    initial commit

---
+++ Lecture3/src/Pattern2/InvertedTriangle.java
@@ -0,0 +1,27 @@
+package Pattern2;
+
+import java.util.Scanner;
+
+public class InvertedTriangle {
+    public static void main(String[] args) {
+        System.out.print("Enter size of Pattern = ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int i = 1;
+        while (i<=n) {
+            int j = 1;
+            while (j<=n-i+1) {
+                System.out.print("*");
+                j++;
+            }
+            int space = 1;
+            while (space<=i) {
+                System.out.print(" ");
+                space++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
