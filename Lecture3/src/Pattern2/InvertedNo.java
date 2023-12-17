a365efed0094ecbefeba9fb18247ae18ddf227e0
Ayush <ayushofficial2323@gmail.com>
2023-12-17 17:36:49 +0530

    initial commit

--- Lecture3/src/Pattern2/InvertedNo.java
+++ Lecture3/src/Pattern2/InvertedNo.java
@@ -2,33 +2,26 @@ package Pattern2;
 
 import java.util.Scanner;
 
-public class InvertedTriangle {
+public class InvertedNo {
     public static void main(String[] args) {
         System.out.print("Enter size of Pattern = ");
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
         int i = 1;
-        while (i<=n) {
+        while (i <= n) {
             int j = 1;
-            while (j<=n-i+1) {
-                System.out.print("*");
+            while (j <= n - i + 1) {
+                System.out.print(n - i + 1);
                 j++;
             }
-//            int space = 1;
-//            while (space<=i) {
-//                System.out.print(" ");
-//                space++;
-//            }
             System.out.println();
             i++;
         }
     }
-//        ****
-//        ***
-//        **
-//        *
-
-
-
 }
+//        4444
+//        333
+//        22
+//        1
+
