477a9c2ae8ae3b36ae7565b2616de3c9381353cc
Ayush <ayushofficial2323@gmail.com>
2023-12-19 22:53:17 +0530

    initial commit

---
+++ Lecture3/src/OperatorAndForLoop/Bitwise.java
@@ -0,0 +1,14 @@
+package OperatorAndForLoop;
+
+public class Bitwise {
+    public static void main(String[] args) {
+        System.out.println(2 & 3);    // and gate
+        System.out.println(2 | 3);    // or gate
+        System.out.println(2 ^ 3);    // xor gate
+        System.out.println(~2);       // not gate
+        System.out.println(34<<1);    //left shift
+        System.out.println(34>>2);    //right shift
+        System.out.println(-4<<1);
+        System.out.println(-4>>1);
+    }
+}
