e662a2f1f529ff786ac69438540e052a8e0ab56d
Ayush <ayushofficial2323@gmail.com>
2023-12-19 23:05:23 +0530

    initial commit

---
+++ Lecture3/src/OperatorAndForLoop/FibonacciNo.java
@@ -0,0 +1,19 @@
+package OperatorAndForLoop;
+
+import java.util.Scanner;
+
+public class FibonacciNo {
+    public static void main(String[] args) {
+        System.out.print("Enter n = ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int a = 1;
+        int b = 1;
+        for (int i=1;i<n;i++) {
+            int c = a + b ;
+            a = b;
+            b = c;
+            System.out.println(a);
+        }
+    }
+}
