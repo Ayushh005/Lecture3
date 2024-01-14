b98ef513e1eef7dbd07e0fa8fcd881888c06a0ba
Ayush <ayushofficial2323@gmail.com>
2024-01-14 22:30:37 +0530

    initial commit

--- Lecture4/src/Functions/NcRUsingFunction.java
+++ Lecture4/src/Functions/NcRUsingFunction.java
@@ -11,16 +11,20 @@ public class NcRUsingFunction {
         }
         return ans;
     }
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int r = s.nextInt();
-
+    public static int ncr(int n,int r) {
         int num = factorial(n);
         int den1 = factorial(r);
         int den2 = factorial(n-r);
 
         int ans = num/(den1*den2);
-        System.out.println(ans);
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int r = s.nextInt();
+
+        int output = ncr(n,r);
+        System.out.println(output);
     }
 }
