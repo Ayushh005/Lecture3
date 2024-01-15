d0b62ba2f43f02db1d5388dd2c5574071ea69771
Ayush <ayushofficial2323@gmail.com>
2024-01-15 22:34:37 +0530

    initial commit

--- Lecture4/src/Array/LargestInArray.java
+++ Lecture4/src/Array/LargestInArray.java
@@ -3,6 +3,16 @@ package Array;
 import java.util.Scanner;
 
 public class LargestInArray {
+    public static int largest(int input[]) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < input.length ; i++) {
+            if (input[i] > max) {
+                max = input[i];
+            }
+        }
+        return max;
+    }
+
     public static int [] input() {
         Scanner s = new Scanner(System.in);
         int size = s.nextInt();
@@ -17,10 +27,13 @@ public class LargestInArray {
         for (int i=0;i<size;i++) {
             System.out.print(input[i] + " ");
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int arr[] = input();
         Print(arr);
+        int largest = largest(arr);
+        System.out.println("Largest element = "+ largest);
     }
 }
