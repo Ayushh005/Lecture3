dbb2d2870715bd80803945a2a0d3797bddd29c85
Ayush <ayushofficial2323@gmail.com>
2024-01-15 21:48:09 +0530

    initial commit

---
+++ Lecture4/src/Functions/Averages.java
@@ -0,0 +1,14 @@
+package Functions;
+
+public class Averages {
+
+    public static void Average(double a,double b) {
+        double avg = (a+b)/2;
+        System.out.println(avg);
+    }
+
+    public static void main(String[] args) {
+        int a = 15, b = 16;
+        Average(a,b);
+    }
+}
