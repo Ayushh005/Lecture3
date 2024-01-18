d58f1b09b71416d79f0ec4a5ac32b14a07358357
Ayush <ayushofficial2323@gmail.com>
2024-01-18 22:54:47 +0530

    initial commit

---
+++ Lecture3/src/Assignment/ReverseOfNo.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class ReverseOfNo {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        int decimal = 0, pow = 1;
+        while (num>0) {
+            int last = num%10;
+            decimal +=last * pow;
+            pow *= 2;
+            num = num/10;
+        }
+        System.out.println(decimal);
+    }
+}
