611674006c6ab07d2b2118c4b22434a9f76c8d2f
Ayush <ayushofficial2323@gmail.com>
2024-01-21 00:16:14 +0530

    initial commit

--- Lecture4/src/Array/Table.java
+++ Lecture4/src/Array/Table.java
@@ -3,17 +3,27 @@ package Array;
 import java.util.Scanner;
 
 public class Table {
-    public static void main(String[] args) {
+    public static int[] input() {
         Scanner s = new Scanner(System.in);
         int size = s.nextInt();
         int input[] = new int[size];
-        for (int i=0;i<size;i++) {
+        for (int i = 0; i < size; i++) {
             input[i] = s.nextInt();
-
-
-            for (i=0;i<size;i++) {
-                System.out.print(input[i]+" ");
-            }
         }
+        return input;
+    }
+    public static void print(int input[]) {
+        int size = input.length;
+        for (int i = 0; i < size; i++) {
+            System.out.print(input[i] + " ");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = input();
+        print(arr);
+
     }
 }
