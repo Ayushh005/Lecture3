47f7d116e9f13edde086f9585de640ba57ca1eee
Ayush <ayushofficial2323@gmail.com>
2024-01-22 22:52:22 +0530

    initial commit

--- Lecture4/src/Array/LinearSearch.java
+++ Lecture4/src/Array/LinearSearch.java
@@ -15,17 +15,13 @@ public class LinearSearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt(); // Number of test cases
-
         for (int i = 0; i < t; i++) {
             int n = scanner.nextInt(); // Size of the array
             int[] arr = new int[n];
-
             for (int j = 0; j < n; j++) {
                 arr[j] = scanner.nextInt(); // Read array elements
             }
-
             int x = scanner.nextInt(); // Value to search for
-
             int result = LinearSearch(arr,x);
             System.out.println(result); // Print the result
         }
