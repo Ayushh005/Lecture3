347fe45c997e5b20a8e8a55f8ad6093719fccca4
Ayush <ayushofficial2323@gmail.com>
2024-01-23 19:09:59 +0530

    initial commit

--- Lecture4/src/Array/PopulateArray.java
+++ Lecture4/src/Array/PopulateArray.java
@@ -3,37 +3,30 @@ package Array;
 import java.util.Scanner;
 
 public class PopulateArray {
-    public static int[] input() {
+    public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
         int size = s.nextInt();
         int input[] = new int[size];
-        return input;
-    }
-    public static void arrange(int [] input, int size) {
+
         int left = 0;
         int right = size-1;
         int counter = 1;
         while (left<=right) {
-            if (counter%2 == 0) {
+            if (counter % 2 == 0) {
                 input[left] = counter;
                 counter += 1;
                 left += 1;
-            }
-            else {
+            } else {
                 input[right] = counter;
-                counter +=1;
-                right-=1;
+                counter += 1;
+                right -= 1;
             }
         }
-    }
-    public static void print(int arrange[]){
-        for (int i=0;i<arrange.length;i++){
-            System.out.print(arrange[i]+" ");
+
+        for (int i=0;i<input.length;i++){
+            System.out.print(input[i]+" ");
         }
         System.out.println();
-    }
-    public static void main(String[] args) {
-        int arr[] = input();
-        print(arr);
     }
 }
