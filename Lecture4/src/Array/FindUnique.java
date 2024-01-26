39381aad73109e67c493167b1a37e599504607c8
Ayush <ayushofficial2323@gmail.com>
2024-01-26 22:32:26 +0530

    initial commit

--- Lecture4/src/Array/FindUnique.java
+++ Lecture4/src/Array/FindUnique.java
@@ -1,28 +1,14 @@
 package Array;
 
 public class FindUnique {
-    public static int Unique(int[] arr) {
-//        int n = arr.length;
-//        for (int i=0;i<=n;i++) {
-//            int j;
-//            for (j=0;j<n;j++) {
-//                if (i != j) {
-//                    if (arr[i]==arr[j]) {
-//                        break;
-//                    }
-//                }
-//            }
-//            if (j==arr.length) {
-//                return arr[i];
-//            }
-//        }
-//        return Integer.MAX_VALUE;
+    public static void Unique(int[] arr) {
+
         int res = arr[0];
         for (int i=1;i<arr.length;i++) {
             res=res^arr[i];
 
         }
-        return res;
+        System.out.println(res);
     }
 
     public static void main(String[] args) {
