89cce02a9f89c4f02257815e9d86775268d46576
Ayush <ayushofficial2323@gmail.com>
2024-01-26 22:49:58 +0530

    initial commit

---
+++ Lecture4/src/Array/FindDublicate.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class FindDublicate {
+    public static void Dublicate(int[] arr){
+        for (int i=0;i<(arr.length-1);i++){
+            for (int j=i+1;j<arr.length;j++){
+                if (arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 3, 4, 5};
+        Dublicate(arr);
+    }
+}
