604e28c4f6124b04e7a5c26ae4757df4ce42b18d
Ayush <ayushofficial2323@gmail.com>
2024-01-29 19:21:34 +0530

    initial commit

--- Lecture4/src/SearchingAndSorting/BinarySearch.java
+++ Lecture4/src/SearchingAndSorting/BinarySearch.java
@@ -1,29 +1,29 @@
 package SearchingAndSorting;
 
 public class BinarySearch {
-    public static void BinarySearch(int[] arr,int x){
+    public static int BinarySearch(int[] arr,int x) {
         int s = 0;
         int e = arr.length-1;
-        int mid = s;
+
         while (s<=e){
+            int mid = (s+e)/2;
 
-            mid = s + (e-s)/2;
-
-            if (arr[mid]>x){
-                e = mid-1;
+            if (arr[mid] > x){
+                e = mid - 1;
             } else if (arr[mid]<x) {
-                s = mid+1;
+                s = mid + 1;
             }
             else {
-                System.out.println(mid);
+                return mid;
             }
         }
-        System.out.println("-1");
+        return -1;
     }
     public static void main(String[] args) {
-        int arr[] = {1,3,7,9,11,12,45};
+        int[] arr = {1,3,7,9,11,12,45};
         int x = 11;
+        int ans = BinarySearch(arr,x);
+        System.out.println("Index : "+ ans);
 
-        BinarySearch(arr,x);
     }
 }
