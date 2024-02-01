b0c7fb5c575d5ca0fc44aa5b5151cb51ad7ae831
Ayush <ayushofficial2323@gmail.com>
2024-02-01 23:51:10 +0530

    initial commit

---
+++ Lecture4/src/Array/SortZeroOne.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.Scanner;
+
+class  SortZeroOne{
+    public static void SortArray(int[] input){
+        int zero = 0;
+        for (int i=0;i< input.length-1;i++){
+            if (input[i]==0){
+                int temp = input[zero];
+                input[zero] = input[i];
+                input[i] = temp;
+                zero++;
+
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int input[] = {0,0,1,0,1,1,0,1};
+        SortArray(input);
+
+    }
+}
