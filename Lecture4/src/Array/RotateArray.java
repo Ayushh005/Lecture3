edfcfd432a3a89192c613dccaee774449e407ca5
Ayush <ayushofficial2323@gmail.com>
2024-02-05 22:42:46 +0530

    initial commit

---
+++ Lecture4/src/Array/RotateArray.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class RotateArray {
+    public static void Rotate(int[] arr,int d){
+        for (int i=0;i< arr.length;i++){
+
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,2,5,3,1,7,6};
+        int d = 2;
+
+
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
