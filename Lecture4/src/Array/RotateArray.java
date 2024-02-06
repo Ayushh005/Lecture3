21f26d0d2c53e60c62011be6dccdb1ed3c87e9d5
Ayush <ayushofficial2323@gmail.com>
2024-02-06 17:13:10 +0530

    initial commit

--- Lecture4/src/Array/RotateArray.java
+++ Lecture4/src/Array/RotateArray.java
@@ -2,14 +2,26 @@ package Array;
 
 public class RotateArray {
     public static void Rotate(int[] arr,int d){
-        for (int i=d-1;i< arr.length;i++){
-
+        int n = arr.length;
+        int[] temp = new int[n];
+        int current = 0;
+        for (int i=d;i< n;i++){
+            temp[current] = arr[i];
+            current++;
+        }
+        for (int i=0;i<d;i++){
+            temp[current] = arr[i];
+            current++;
+        }
+        for (int i=0;i<n;i++){
+            arr[i] = temp[i];
         }
     }
     public static void main(String[] args) {
-        int[] arr = {4,2,5,3,1,7,6};
+        int[] arr = {1,2,3,4,5};
         int d = 2;
 
+        Rotate(arr,d);
 
         for (int i=0;i< arr.length;i++){
             System.out.print(arr[i]+" ");
