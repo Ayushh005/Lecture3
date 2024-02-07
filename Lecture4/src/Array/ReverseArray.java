8e58cc5a0bc5de5e116f649f3e3de86d2ef18708
Ayush <ayushofficial2323@gmail.com>
2024-02-07 21:11:10 +0530

    initial commit

--- Lecture4/src/Array/ReverseArray.java
+++ Lecture4/src/Array/ReverseArray.java
@@ -1,16 +1,31 @@
 package Array;
 
 public class ReverseArray {
+    public static void rotate(int[] arr){
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        for (int i=0;i<n;i++){
+            while (start<end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
     public static void main(String[] args) {
         int [] arr = new int[]{1,2,3,4,5};
         System.out.println("Original Array:");
         for (int i=0;i< arr.length;i++) {
             System.out.print(arr[i] + " ");
         }
+        rotate(arr);
         System.out.println();
         System.out.println("Array in Reverse Order:");
-        for (int i= arr.length-1;i>=0;i--) {
-            System.out.print(arr[i] + " ");
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
         }
     }
 }
