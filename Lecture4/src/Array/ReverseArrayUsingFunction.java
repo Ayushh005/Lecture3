66a3d64dfb3640423408e348de6765cf404a670e
Ayush <ayushofficial2323@gmail.com>
2024-02-07 18:47:48 +0530

    initial commit

--- Lecture4/src/Array/ReverseArrayUsingFunction.java
+++ Lecture4/src/Array/ReverseArrayUsingFunction.java
@@ -3,26 +3,28 @@ package Array;
 import java.util.Scanner;
 
 public class ReverseArrayUsingFunction {
-    public static int[] arr() {
 
-        Scanner s = new Scanner(System.in);
-        int size = s.nextInt();
-        int [] arr = new int[size];
-        for (int i=0;i< arr.length;i++) {
-            arr[i] = s.nextInt();
+    public static void rotate(int[] arr){
+        int n = arr.length;
+        int start = 1;
+        int last = n-1;
+        for (int i=0;i< n;i++){
+            while (start < last){
+                int temp = arr[start];
+                arr[start] = arr[last];
+                arr[last] = temp;
+            }
+            start++;
+            last--;
         }
-        return arr;
-    }
-    public static void print (int arr[]) {
-        int size = arr.length;
-        for (int i=0;i<size;i++) {
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
     }
 
     public static void main(String[] args) {
-        int arr[] = arr();
-        print(arr);
+        int[] arr = {1,2,3,4,5};
+        rotate(arr);
+        for (int i=0;i< arr.length;i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
     }
 }
