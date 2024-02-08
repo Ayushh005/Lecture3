5fe28a5606356fbcf0eb62f32ca6cde86aacc7d0
Ayush <ayushofficial2323@gmail.com>
2024-02-08 22:17:11 +0530

    initial commit

--- Lecture4/src/Array/PopulateArray.java
+++ Lecture4/src/Array/PopulateArray.java
@@ -4,37 +4,39 @@ package Array;
 import java.util.Scanner;
 
 public class PopulateArray {
-    public static int[] input() {
+    public static int[] input(){
         Scanner s = new Scanner(System.in);
-        int size = s.nextInt();
-        int input[] = new int[size];
+        int n = s.nextInt();
+        int[] input = new int[n];
         return input;
     }
-
-    public static void arrange(int[] input) {
-        int size = input.length; // Use the length of the provided array
+    public static void populateArray(int[] input){
+        int n = input.length;
         int start = 0;
-        int end = size - 1;
+        int end = n-1;
         int counter = 1;
-        while (start <= end) {
-            if (counter % 2 == 0) {
+        while (start<=end){
+            if (counter%2==0){
                 input[start] = counter;
-                counter += 1;
-                start += 1;
-            } else {
+                counter++;
+                start++;
+            }
+            else {
                 input[end] = counter;
-                counter += 1;
-                end -= 1;
+                counter++;
+                end--;
             }
         }
-        for (int i = 0; i < input.length; i++) {
-            System.out.print(input[i] + " "); // Use the provided array, not input()
-        }
-        System.out.println();
     }
 
     public static void main(String[] args) {
+        System.out.print("Enter Array Size :");
         int arr[] = input();
-        arrange(arr);
+        populateArray(arr);
+
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
+
 }
