d9a534a4b593a303c73d57e4f4abaa32f6143297
Ayush <ayushofficial2323@gmail.com>
2024-02-09 22:44:39 +0530

    initial commit

---
+++ Lecture4/src/SearchingAndSorting/InsertionSort.java
@@ -0,0 +1,25 @@
+package SearchingAndSorting;
+
+public class InsertionSort {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int j = i - 1;
+            int temp = arr[i];
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 7, 8, 3, 9, 5};
+        sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
