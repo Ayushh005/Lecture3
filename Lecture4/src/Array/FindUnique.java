9479f419ee03d3d19eea21b16ccfd89de7f61349
Ayush <ayushofficial2323@gmail.com>
2024-02-12 22:42:38 +0530

    initial commit

--- Lecture4/src/Array/FindUnique.java
+++ Lecture4/src/Array/FindUnique.java
@@ -12,7 +12,7 @@ public class FindUnique {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,1,5,5,6,7,7,};
+        int arr[] = {1,1,5,5,6,7,7};
         Unique(arr);
     }
 }
