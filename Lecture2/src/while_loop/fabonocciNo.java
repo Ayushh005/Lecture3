9fe78fc0c629d40d19282d4ee752f530abb4a827
Ayush <ayushofficial2323@gmail.com>
2024-02-15 22:33:06 +0530

    commmit

--- Lecture2/src/while_loop/fabonocciNo.java
+++ Lecture2/src/while_loop/fabonocciNo.java
@@ -6,8 +6,8 @@ public class fabonocciNo {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int a = 0;
-        int b = 1;
+        int a = 1;
+        int b = 2;
         for (int i=1;i<=n;i++){
             System.out.print(a+" ");
             int c = a + b;
