47fd2a1d31f1bb9e576b5159babaa4e1a2963cdf
Ayush <ayushofficial2323@gmail.com>
2024-02-16 22:58:05 +0530

    initial commit

--- Lecture4/src/SearchingAndSorting/SelectionSort.java
+++ Lecture4/src/SearchingAndSorting/SelectionSort.java
@@ -5,7 +5,7 @@ public class SelectionSort {
         for (int i=0;i< arr.length-1;i++){
             int min = arr[i];
             int minIndex = i;
-            for (int j=i;j< arr.length;j++){
+            for (int j=i+1;j< arr.length;j++){
                 if (min > arr[j]){
                     min = arr[j];
                     minIndex = j;
