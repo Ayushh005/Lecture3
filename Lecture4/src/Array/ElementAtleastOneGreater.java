c4216b078529600dc118da5e6030c9280186a6e0
Ayush <ayushofficial2323@gmail.com>
2024-02-22 18:01:07 +0530

    initial commit

---
+++ Lecture4/src/Array/ElementAtleastOneGreater.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class ElementAtleastOneGreater {
+    public static void countGreater(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i=0;i< arr.length;i++){
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        int count = 0;
+        for (int i=0;i< arr.length;i++){
+            if (arr[i] == max){
+                count++;
+            }
+        }
+        int ans = arr.length - count;
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,4,11,8,2,10,9,11};
+        System.out.print("No. of Elements having atleast One greater then itself = ");
+        countGreater(arr);
+    }
+}
