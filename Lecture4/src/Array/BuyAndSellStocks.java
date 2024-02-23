9689a97d4c7015f5f266ca368bed03e5b2f85374
Ayush <ayushofficial2323@gmail.com>
2024-02-23 13:09:32 +0530

    initial commit

---
+++ Lecture4/src/Array/BuyAndSellStocks.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class BuyAndSellStocks {
+    public static void profit(int[] arr) {
+        int buy = Integer.MAX_VALUE;
+        int profit = 0;
+        int maxProfit = 0;
+        for (int i = 0;i< arr.length;i++) {
+            if (buy > arr[i]) {
+                buy = arr[i];
+            }
+            else{
+                profit = arr[i]-buy;
+            }
+            if (profit>maxProfit){
+                maxProfit = profit;
+            }
+        }
+        System.out.println(maxProfit);
+    }
+    public static void main(String[] args) {
+        int[] arr = {44,30,24,32,35,30,40,38,15};
+
+        profit(arr);
+    }
+}
