277939f69b35197c220fc77dc53c1bc7247dc95d
Ayush <ayushofficial2323@gmail.com>
2024-02-24 18:40:24 +0530

    initial commit

---
+++ Lecture4/src/Strings/PalindromeString.java
@@ -0,0 +1,19 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static boolean isPalindrome(String str){
+        for (int i=0;i<str.length()/2;i++){
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+    }
+}
