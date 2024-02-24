71f712aa69cd3bab56ed438dd550ecc9287c74f5
Ayush <ayushofficial2323@gmail.com>
2024-02-24 19:13:49 +0530

    initial commit

---
+++ Lecture4/src/Strings/ShortestPath.java
@@ -0,0 +1,30 @@
+package Strings;
+
+public class ShortestPath {
+    public static float getShortestPath(String path){
+        int x = 0, y = 0;
+        for (int i=0;i<path.length();i++){
+            char dir = path.charAt(i);
+            if (dir == 'N'){
+                y++;
+            } else if (dir == 'S') {
+                y--;
+            }
+            else if (dir == 'W'){
+                x--;
+            }
+            else {
+                x++;
+            }
+        }
+        int x2 = x*x;
+        int y2 = y*y;
+        return (float) Math.sqrt(x2 + y2);
+    }
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+
+        System.out.println(getShortestPath(path));
+
+    }
+}
