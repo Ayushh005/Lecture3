fa3aa6ebebcad242e4203ad7c7655ec6c1036104
Ayush <ayushofficial2323@gmail.com>
2024-02-27 22:56:34 +0530

    initial commit

--- Lecture4/src/Array/Table.java
+++ Lecture4/src/Array/Table.java
@@ -3,26 +3,21 @@ package Array;
 import java.util.Scanner;
 
 public class Table {
-    public static int[] input() {
-        Scanner s = new Scanner(System.in);
-        int size = s.nextInt();
-        int input[] = new int[size];
-        for (int i = 0; i < size; i++) {
-            input[i] = s.nextInt();
-        }
-        return input;
-    }
-    public static void print(int input[]) {
-        int size = input.length;
-        for (int i = 0; i < size; i++) {
-            System.out.print(input[i] + " ");
+    public static void print(int arr[]) {
+        for (int i=0;i<arr.length;i++){
+            int j = 1;
+            while (j<=10){
+                System.out.println(arr[i]+"x"+j+"="+arr[i]*j);
+                j++;
+            }
+            System.out.println();
         }
 
     }
 
 
     public static void main(String[] args) {
-        int arr[] = input();
+        int arr[] = {6,7,8,9,10};
         print(arr);
 
     }
