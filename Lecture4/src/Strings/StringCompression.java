e5887baec3848508ab9cbc34a9db0a51becf22fd
Ayush <ayushofficial2323@gmail.com>
2024-02-27 18:30:06 +0530

    initial commit

---
+++ Lecture4/src/Strings/StringCompression.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class StringCompression {
+    public static String compress(String str){
+        String newStr = "";
+        for (int i=0;i<str.length();i++){
+            Integer count = 1;
+            while (i<str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if (count > 1){
+                newStr += count.toString();
+            }
+        }
+        return newStr;
+    }
+    public static void main(String[] args) {
+        String str = "aaabbccdd";
+        System.out.println(compress(str));
+    }
+}
