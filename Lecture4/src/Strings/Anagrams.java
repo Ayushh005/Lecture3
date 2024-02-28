0f02178f604e2e940639aa34e2ac4dd14ce5fea1
Ayush <ayushofficial2323@gmail.com>
2024-02-28 22:50:01 +0530

    initial commit

---
+++ Lecture4/src/Strings/Anagrams.java
@@ -0,0 +1,34 @@
+package Strings;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Anagrams {
+    public static void anagram(String s1,String s2){
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        if (s1.length() == s2.length()){
+            char[] str1 = s1.toCharArray();
+            char[] str2 = s2.toCharArray();
+
+            // sort the array
+            Arrays.sort(str1);
+            Arrays.sort(str2);
+
+            boolean result = Arrays.equals(str1,str2);
+            if (result){
+                System.out.println(s1 + " and " + s2 + " are anagram of each other.");
+            }
+            else {
+                System.out.println(s1 + " and " + s2 + " are not anagram of each other.");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String s1 = "Race";
+        String s2 = "Care";
+
+        anagram(s1,s2);
+    }
+}
