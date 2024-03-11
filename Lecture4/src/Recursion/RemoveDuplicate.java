28e2ff57f327c98c37915262fee6f0ba660856d2
Ayush <ayushofficial2323@gmail.com>
2024-03-11 18:51:44 +0530

    initial commit

--- Lecture4/src/Recursion/RemoveDuplicate.java
+++ Lecture4/src/Recursion/RemoveDuplicate.java
@@ -1,22 +1,22 @@
 package Recursion;
 
 public class RemoveDuplicate {
-    public static void remove(String str,int i,StringBuilder newStr,boolean[] map){
-        if (i == str.length()){
+    public static void remove(String str, int i, StringBuilder newStr, boolean[] map) {
+        if (i == str.length()) {
             System.out.println(newStr);
             return;
         }
-        char current = newStr.charAt(i);
-        if (map[current-'a'] == true){
-            remove(str,i+1,newStr,map);
-        }
-        else {
-            map[current-'a'] == true;
-            remove(str,i+1,newStr.append(current),map);
+        char current = str.charAt(i);
+        if (map[current - 'a']) {
+            remove(str, i + 1, newStr, map);
+        } else {
+            map[current - 'a'] = true;
+            remove(str, i + 1, newStr.append(current), map);
         }
     }
+
     public static void main(String[] args) {
         String str = "appnacollege";
-        System.out.println(str,0,StringBuilder(""),new boolean[26]);
+        remove(str, 0, new StringBuilder(), new boolean[26]);
     }
 }
