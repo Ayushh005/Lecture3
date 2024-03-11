55446f1658923369266de00e9c2e72a6ab2086d2
Ayush <ayushofficial2323@gmail.com>
2024-03-11 18:53:35 +0530

    initial commit

--- Lecture4/src/Recursion/CountAndSay.java
+++ Lecture4/src/Recursion/CountAndSay.java
@@ -4,15 +4,15 @@ public class CountAndSay {
     public static String countAndSay(int n) {
         if (n==1)  return "1";
 
-        String s = countAndSay(n-1);
+        String say = countAndSay(n-1);
 
         int count = 0;
         StringBuilder ans = new StringBuilder();
 
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<say.length();i++){
             count++;
-            if(i==s.length()-1 || s.charAt(i)!=s.charAt(i+1)){
-                ans.append(count).append(s.charAt(i));
+            if(i==say.length()-1 || say.charAt(i)!=say.charAt(i+1)){
+                ans.append(count).append(say.charAt(i));
                 count=0;
             }
         }
