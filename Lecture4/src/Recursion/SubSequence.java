7a75e028849fe0474736d83f7f213c597db25a98
Ayush <ayushofficial2323@gmail.com>
2024-03-15 23:10:27 +0530

    initial commit

--- Lecture4/src/Recursion/SubSequence.java
+++ Lecture4/src/Recursion/SubSequence.java
@@ -2,20 +2,18 @@ package Recursion;
 
 public class SubSequence {
     public static String[] findSubsequence(String str) {
-        if (str.length() == 0) {
+        if (str.length() == 0){
             String[] ans = {""};
             return ans;
         }
-        String[] smallAns = findSubsequence(str.substring(1));
-        String[] ans = new String[2 * smallAns.length];
-        int k = 0;
-        for (int i = 0; i < smallAns.length; i++) {
-            ans[k] = smallAns[i];
-            k++;
+        String smallAns[] = findSubsequence(str.substring(1));
+        String ans[] = new String[2 * smallAns.length];
+
+        for (int i=0;i<smallAns.length;i++){
+            ans[i] = smallAns[i];
         }
-        for (int i = 0; i < smallAns.length; i++) {
-            ans[k] = str.charAt(0) + smallAns[i];
-            k++;
+        for (int i=0;i< smallAns.length;i++){
+            ans[i + smallAns.length] = str.charAt(0) + smallAns[i];
         }
         return ans;
     }
