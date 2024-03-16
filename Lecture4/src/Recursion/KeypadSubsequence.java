406e97e0f08468a4885e29f8d23f51ba85b77c26
Ayush <ayushofficial2323@gmail.com>
2024-03-16 23:26:20 +0530

    initial commit

--- Lecture4/src/Recursion/KeypadSubsequence.java
+++ Lecture4/src/Recursion/KeypadSubsequence.java
@@ -1,54 +1,39 @@
 package Recursion;
 
 public class KeypadSubsequence {
-    public static String[] singleDigit(int n){
-        if (n<=1 || n>10){
+    public static String[] singleDigit(int n) {
+        if (n <= 1 || n > 10) {
             return new String[0];
         }
-        if (n==2){
-            String[] output = {"a","b","c"};
-            return output;
-        }
-        else if (n==3){
-            String[] output = {"d","e","f"};
-            return output;
-        }
-        else if (n==4){
-            String[] output = {"g","h","i"};
-            return output;
-        }
-        else if (n==5){
-            String[] output = {"j","k","l"};
-            return output;
-        }
-        else if (n==6){
-            String[] output = {"m","n","o"};
-            return output;
-        }
-        else if (n==7){
-            String[] output = {"p","q","r","s"};
-            return output;
-        }
-        else if (n==8){
-            String[] output = {"t","u","v"};
-            return output;
-        }
-        else {
-            String[] output = {"w","x","y","z"};
-            return output;
+        if (n == 2) {
+            return new String[]{"a", "b", "c"};
+        } else if (n == 3) {
+            return new String[]{"d", "e", "f"};
+        } else if (n == 4) {
+            return new String[]{"g", "h", "i"};
+        } else if (n == 5) {
+            return new String[]{"j", "k", "l"};
+        } else if (n == 6) {
+            return new String[]{"m", "n", "o"};
+        } else if (n == 7) {
+            return new String[]{"p", "q", "r", "s"};
+        } else if (n == 8) {
+            return new String[]{"t", "u", "v"};
+        } else {
+            return new String[]{"w", "x", "y", "z"};
         }
     }
-    public static String[] keypad(int n){
-        if (n==0){
-            String[] output = {""};
-            return output;
+
+    public static String[] keypad(int n) {
+        if (n == 0) {
+            return new String[]{""};
         }
-        String smallNumberArray[] = keypad(n/10);
-        String singleDigitOutput[] = singleDigit(n%10);
-        String output[] = new String[singleDigitOutput.length + smallNumberArray.length];
+        String[] smallNumberArray = keypad(n / 10);
+        String[] singleDigitOutput = singleDigit(n % 10);
+        String[] output = new String[singleDigitOutput.length * smallNumberArray.length];
         int k = 0;
-        for (int i=0;i<singleDigitOutput.length;i++){
-            for (int j=0;j<smallNumberArray.length;j++){
+        for (int i = 0; i < singleDigitOutput.length; i++) {
+            for (int j = 0; j < smallNumberArray.length; j++) {
                 output[k] = smallNumberArray[j] + singleDigitOutput[i];
                 k++;
             }
@@ -57,7 +42,10 @@ public class KeypadSubsequence {
     }
 
     public static void main(String[] args) {
-        int n = 2;
-        System.out.println(keypad(n));
+        int n = 23;
+        String[] result = keypad(n);
+        for (String s : result) {
+            System.out.println(s);
+        }
     }
 }
