e1f76e50eda5601ead01060dbe5543c54b915eb6
Ayush <ayushofficial2323@gmail.com>
2024-03-18 22:25:54 +0530

    initial commit

--- Lecture4/src/TwoDimensionArray/WaveFormMatrix.java
+++ Lecture4/src/TwoDimensionArray/WaveFormMatrix.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 
 public class WaveFormMatrix {
 
-    public static void main(String[] args) {             //   solve the error
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int m = s.nextInt();
         int n = s.nextInt();
-        int[][] arr =   new int[m][n];
+        int[][] arr = new int[m][n];
 
-        for (int i=0;i<m;i++){
-            for (int j=0;j<n;j++){
-                arr[m][n] = s.nextInt();
+        // Input Reading
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                arr[i][j] = s.nextInt();
             }
         }
 
-        for (int i=0;i<m;i++){
-            if (i%2==0){
-                for (int j=0;j<n;j++){
-                    System.out.println(arr[m][n]+" ");
+        // Printing
+        for (int i =0;i<m;i++) {
+            if (i%2 == 0) {
+                for (int j=0;j<n;j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            } else {
+                for (int j=n-1;j>=0;j--) {
+                    System.out.print(arr[i][j] + " ");
                 }
             }
-            else {
-                for (int j=n-1;j>=0;j--){
-                    System.out.println(arr[i][j]+" ");
-                }
-            }
+            System.out.println(); // Print a new line after each row
         }
     }
-
-    
 }
