01d4fc64eca2ad55a049f4eb8de8ff57bedd7d0d
Ayush <ayushofficial2323@gmail.com>
2024-03-19 19:15:26 +0530

    initial commit

---
+++ Lecture4/src/Strings/SubstrngP1.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class SubstrngP1 {
+    public static void printAllSubstring(String str){
+        for (int i=0;i<str.length();i++){
+            for (int j=i+1;j<=str.length();j++){
+                System.out.println(str.substring(i,j));
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String str = "abcd";
+        printAllSubstring(str);
+    }
+}
