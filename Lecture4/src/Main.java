82d9542858f52118948e5883af948b17e1639dbf
Ayush <ayushofficial2323@gmail.com>
2024-04-16 22:35:10 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -4,34 +4,19 @@ import java.awt.desktop.SystemSleepEvent;
 import java.util.Scanner;
 
 public class Main {
-    public static int[] ans(int[] arr) {
-        int n = arr.length;
-        int start = 0;
-        int last = n-1;
-        int counter = 1;
-        while (start <= last) {
-            if (counter % 2 == 0) {
-                arr[start] = counter;
-                start++;
-                counter++;
-            }
-            else {
-                arr[last] = counter;
-                last--;
-                counter++;
+    public static boolean isPalindrome(String str){
+        int n = str.length();
+        for (int i=0;i<str.length()/2;i++){
+            if (str.charAt(i) != str.charAt(n-i-1)){
+                return false;
             }
         }
-        System.out.println("Array elements:");
-        for (int i = 1; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        return arr;
+        return true;
     }
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int size = s.nextInt();
-        int[] arr = new int[size];
-        ans(arr);
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
 
     }
 }
