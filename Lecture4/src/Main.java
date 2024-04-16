2c8af84e360e43624f1d8e18a601ebd4aedb27ee
Ayush <ayushofficial2323@gmail.com>
2024-04-16 22:57:00 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -4,20 +4,21 @@ import java.awt.desktop.SystemSleepEvent;
 import java.util.Scanner;
 
 public class Main {
-    public static boolean isPalindrome(String str){
+    public static String removeChar(String str, char ch){
+        StringBuilder ans = new StringBuilder();
         int n = str.length();
-        for (int i=0;i<str.length()/2;i++){
-            if (str.charAt(i) != str.charAt(n-i-1)){
-                return false;
+        for (int i=0;i<n;i++){
+            if (str.charAt(i) != 'a'){
+                ans.append(str.charAt(i));
             }
         }
-        return true;
+        return ans.toString();
     }
 
     public static void main(String[] args) {
-        String str = "racecar";
-        System.out.println(isPalindrome(str));
-
+        String str = "america";
+        char ch = 'a';
+        System.out.println(removeChar(str,ch));
     }
 }
 
