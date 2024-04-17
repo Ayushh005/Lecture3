961cef6c543353b448b9d25cab83272827789f2e
Ayush <ayushofficial2323@gmail.com>
2024-04-17 23:18:01 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,19 +1,23 @@
-
-
 import java.awt.desktop.SystemSleepEvent;
 import java.util.Scanner;
 
 public class Main {
-    public static String removeChar(String str){
+    public static String compress(String str){
         int n = str.length();
         if (n == 0)  return str;
 
         StringBuilder ans = new StringBuilder();
-        ans.append(str.charAt(0));
 
-        for (int i=1;i<n;i++){
-            if (str.charAt(i) != str.charAt(i-1)){
-                ans.append(str.charAt(i));
+        for (int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            int count = 1;
+            while (i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            ans.append(ch);
+            if (count > 1) {
+                ans.append(count);
             }
         }
         return ans.toString();
@@ -21,7 +25,6 @@ public class Main {
 
     public static void main(String[] args) {
         String str = "aabbbcddeeefggh";
-        System.out.println(removeChar(str));
+        System.out.println(compress(str));
     }
 }
-
