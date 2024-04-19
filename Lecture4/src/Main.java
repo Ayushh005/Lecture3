6c233ecea8a800fdd73983c3926f54d189759bea
Ayush <ayushofficial2323@gmail.com>
2024-04-19 22:38:19 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -2,28 +2,17 @@ import java.awt.desktop.SystemSleepEvent;
 import java.util.Scanner;
 
 public class Main {
-    public static void reverse(int[] arr,int s, int e){
-        while (s < e){
-            int temp = arr[s];
-            arr[s] = arr[e];
-            arr[e] = temp;
-            s++;
-            e--;
+    public static int count(String str){
+        int i = 0;
+        int count = 0;
+        while (i<str.length() && str.length() != ' '){
+            count++;
         }
+        return count;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8};
-        int s = 0;
-        int e = arr.length-1;
-        int k = 3;
-
-        reverse(arr,s,e);
-        reverse(arr,s,k-1);
-        reverse(arr,k,e);
-
-        for (int i=0;i< arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
+        String str = "i am the best in this entire world";
+        System.out.println(count(str));
     }
 }
