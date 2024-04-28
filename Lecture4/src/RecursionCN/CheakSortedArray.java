ddc502a7a9e797d8ea3aca8dbfacaa5326f00341
Ayush <ayushofficial2323@gmail.com>
2024-04-28 22:46:48 +0530

    initial commit

---
+++ Lecture4/src/RecursionCN/CheakSortedArray.java
@@ -0,0 +1,18 @@
+package RecursionCN;
+
+public class CheakSortedArray {
+    public static boolean isSorted(int[] arr,int startIndex){
+        if (startIndex >= arr.length-1) {
+            return true;
+        }
+        if (arr[startIndex] > arr[startIndex + 1]){
+             return false;
+        }
+        boolean smallAns = isSorted(arr,startIndex + 1);
+        return smallAns;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,2,6,7,9};
+        System.out.println(isSorted(arr,0));
+    }
+}
