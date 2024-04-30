0e8f2a040df29e165685eb74695239b2cab50925
Ayush <ayushofficial2323@gmail.com>
2024-04-30 22:44:32 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -22,7 +22,7 @@ public class Main {
     }
     public static void main(String[] args) {
         int[] arr = {10,8,9,8,5,6,4,3,8};
-        int x = 8;
+        int x = 5;
         System.out.println(search(arr,x,0));
     }
 }
