7994a44b2f7b094243f313a43d77341ce592aa0e
Ayush <ayushofficial2323@gmail.com>
2024-04-30 22:23:02 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -2,16 +2,21 @@ import java.awt.desktop.SystemSleepEvent;
 import java.util.Scanner;
 
 public class Main {
-    public static int sum(int arr[],int startIndex){
-        if (startIndex == arr.length) {
-            return 0;
-        }
-        int ans = sum(arr,startIndex+1);
-        return arr[startIndex] + ans;
+    public static int search(int[] arr,int x){
+        return search(arr,x,0);
     }
-
+    private static int search(int[] arr,int x,int startIndex){
+        if (startIndex == arr.length) {
+            return -1;
+        }
+        if (arr[startIndex] == x) {
+            return startIndex;
+        }
+        return search(arr, x,startIndex + 1);
+    }
     public static void main(String[] args) {
-        int[] arr = {4,5,9,7,6,3,5,6};
-        System.out.println(sum(arr,0));
+        int[] arr = {10,8,9,8,5,6,4,3};
+        int x = 8;
+        System.out.println(search(arr,x,0));
     }
 }
