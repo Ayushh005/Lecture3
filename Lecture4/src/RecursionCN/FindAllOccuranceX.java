5fdec2af78ef4599f3c1953d7259da5bbc57c44b
Ayush <ayushofficial2323@gmail.com>
2024-05-03 23:13:56 +0530

    initial commit

---
+++ Lecture4/src/RecursionCN/FindAllOccuranceX.java
@@ -0,0 +1,29 @@
+package RecursionCN;
+
+public class FindAllOccuranceX {
+    public static int[] find(int[]arr,int x,int startIndex){
+        if (startIndex == arr.length) {
+            return new int[0];
+        }
+        int smallOutput[] = find(arr,x,startIndex+1);
+        if (arr[startIndex] == x) {
+            int[] output = new int[smallOutput.length + 1];
+            output[0] = startIndex;
+            for (int i=0;i<smallOutput.length;i++){
+                output[i+1] = smallOutput[i];
+            }
+            return output;
+        }
+        else {
+            return smallOutput;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {10,8,9,8,5,6,4,3,8};
+        int x = 8;
+        int[] ans = find(arr,8,0);
+        for (int i : ans) {
+            System.out.print(i + " ");
+        }
+    }
+}
