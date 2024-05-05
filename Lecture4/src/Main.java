2fcf68b26412c71ef1a5598d358d2134bf36acac
Ayush <ayushofficial2323@gmail.com>
2024-05-05 22:38:35 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -3,29 +3,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static int[] search(int[] arr,int x,int startIndex){
-        if (startIndex == arr.length) {
-            return new int[0];
-        }
-        int smallAns[] = search(arr,x,startIndex+1);
-
-        if (arr[startIndex] == x) {
-            int[] ans = new int[smallAns.length + 1];
-            ans[0] = startIndex;
-            for (int i=0;i< smallAns.length;i++){
-                ans[i+1] = smallAns[i];
-            }
-            return ans;
-        }else{
-            return smallAns;
-        }
-    }
     public static void main(String[] args) {
-        int[] arr = {10,8,9,8,5,6,4,3,8};
-        int x = 12;
-        int[] ans = search(arr,8,0);
-        for (int i : ans) {
-            System.out.print(i + " ");
-        }
+        int p = 10000;
+        int r = 5;
+        int t = 2;
+
+        int SI = p * r * t / 100;
+        System.out.println(SI);
     }
 }
