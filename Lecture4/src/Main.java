ab5054ee9d554271473637c2f2c286615ac3ae82
Ayush <ayushofficial2323@gmail.com>
2024-05-07 13:54:06 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,14 +1,20 @@
-import java.awt.desktop.SystemSleepEvent;
 import java.util.Scanner;
 
 public class Main {
+    public static boolean check(String str, int s, int e) {
+        if (s >= e) {
+            return true;
+        }
+        if (str.charAt(s) != str.charAt(e - 1)) {
+            return false;
+        }
+        return check(str, s + 1, e - 1);
+    }
 
     public static void main(String[] args) {
-        int p = 10000;
-        int r = 5;
-        int t = 2;
-
-        int SI = p * r * t / 100;
-        System.out.println(SI);
+        String str = "racecar";
+        int s = 0; // Start from index 0
+        int e = str.length() - 1; // Index of the last character
+        System.out.println(check(str, s, e) ? "Palindrome" : "Not a palindrome");
     }
 }
