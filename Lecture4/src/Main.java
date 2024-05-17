d41a41ca237642dbb14862218e15ac142f90893b
Ayush <ayushofficial2323@gmail.com>
2024-05-17 22:34:08 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,48 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void mergeSort(int[] input){
-        if (input.length <= 1){
+
+    public static void merge(int[] arr,int si,int ei){
+        if (arr.length <= 1){
             return;
         }
-        int mid = input.length/2;
-        int[] a = new int[mid];
-        int[] b = new int[input.length - mid];
-        for (int i = 0;i < mid;i++){
-            a[i] = input[i];
-        }
-        for (int i = mid;i < input.length;i++){
-            b[i - mid] = input[i]; // Corrected index
-        }
-        mergeSort(a);
-        mergeSort(b);
-        merge(input,a,b);
+        int mid = (si + ei)/2;
+        merge(arr,si,mid);
+        merge(arr,mid+1,ei);
+
     }
-    public static void merge(int[] output,int[] a ,int[] b){
-        int i=0,j=0,k=0;
-        while (i < a.length && j < b.length){
-            if (a[i] < b[j]){
-                output[k] = a[i];
-                i++;k++;
-            }
-            else {
-                output[k] = b[j];
-                j++;k++;
-            }
-        }
-        while (i < a.length){
-            output[k] = a[i];
-            i++;k++;
-        }
-        while (j < b.length) {
-            output[k] = b[j];
-            j++;k++;
-        }
-    }
+    public static void mergeSort(int[] arr)
 
     public static void main(String[] args) {
         int[] arr = {3, 6, 8, 7, 9, 4, 1, 5 };
-        mergeSort(arr);
+        merge(arr,0, arr.length-1);
         for(int num : arr) {
             System.out.print(num + " ");
         }
