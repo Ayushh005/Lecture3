3d74228db900fcaf044985713ba520b1f0e270cc
Ayush <ayushofficial2323@gmail.com>
2024-05-18 12:57:07 +0530

    initial commit

--- Lecture4/src/Main.java
+++ Lecture4/src/Main.java
@@ -1,21 +1,49 @@
-import java.util.Scanner;
+
 
 public class Main {
 
-    public static void merge(int[] arr,int si,int ei){
+    public static void mergeSort(int[] arr){
         if (arr.length <= 1){
             return;
         }
-        int mid = (si + ei)/2;
-        merge(arr,si,mid);
-        merge(arr,mid+1,ei);
+        int mid = arr.length/2;
+        int[] a = new int[mid];
+        int[] b = new int[arr.length - mid];
+        for (int i=0;i<mid;i++){
+            a[i] = arr[i];
+        }
+        for (int i=mid;i< arr.length;i++){
+            b[i-mid] = arr[i];
+        }
+        mergeSort(a);
+        mergeSort(b);
+        merge(arr,a,b);
 
     }
-    public static void mergeSort(int[] arr)
+    public static void merge(int[] output,int[] a ,int[] b){
+        int i=0,j=0,k=0;
+        while(i<a.length && j<b.length){
+            if (a[i] < b[j]){
+                output[k] = a[i];
+                i++;k++;
+            }else {
+                output[k] = b[j];
+                j++;k++;
+            }
+            while (i < a.length) {
+                output[k] = a[i];
+                i++;k++;
+            }
+            while (j < b.length) {
+                output[k] = b[j];
+                j++;k++;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int[] arr = {3, 6, 8, 7, 9, 4, 1, 5 };
-        merge(arr,0, arr.length-1);
+        mergeSort(arr);
         for(int num : arr) {
             System.out.print(num + " ");
         }
